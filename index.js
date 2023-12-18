f5789ca6b06644f4bdfc7f09dc5a0198eecddff6
parkertimmins <parkertimmins@gmail.com>
2023-12-18 09:33:49 -0600

    add moon

--- index.js
+++ index.js
@@ -1,5 +1,113 @@
 
 
+// https://en.wikipedia.org/wiki/Julian_day
+// https://github.com/mourner/suncalc/blob/master/suncalc.js 
+const millisPerDay = 1000 * 60 * 60 * 24
+const daysPerCentury = 36525
+const j2000Date = Date.UTC(2000, 0, 1, 12, 0, 0)
+const j2000jd = 2451545
+const toJulianCenturies = (jd) =>  (jd - j2000jd) / daysPerCentury
+const toJd = (date) => (date - j2000Date) / millisPerDay + j2000jd
+const mod = (m, n) => ((m%n)+n)%n 
+const rad = (deg) => deg * Math.PI / 180
+const degree = (radian) => radian * (180 / Math.PI)
+const sin = (deg) => Math.sin(rad(deg)), 
+      cos = (deg) => Math.cos(rad(deg)), 
+      tan = (deg) => Math.tan(rad(deg)), 
+      acos = (x) => degree(Math.acos(x)), 
+      asin = (x) => degree(Math.asin(x)), 
+      atan = (x) => degree(Math.atan(x)), 
+      atan2 = (x, y) => degree(Math.atan2(x, y)), 
+      PI = Math.PI; 
+
+
+
+
+
+
+const EARTH_RADIUS_KM = 6378.14
+
+// Originally from most recent Astronomical Almanac, at least 1999-2015
+// An Alternative Lunar Ephemeris Model
+// https://caps.gsfc.nasa.gov/simpson/pubs/slunar.pdf
+export class Moon {
+
+    // Main public method for 
+    static eclipLatLong(jd) {
+        const t = toJulianCenturies(jd)
+        const eclipLongBase = Moon.eclipticLongBase(t)
+        const eclipLatBase = Moon.eclipticLatBase(t)
+        //const [eclipLat, eclipLong] = Moon.precessionCorrection(t, eclipLatBase, eclipLongBase)
+        return {
+            long: eclipLongBase,
+            lat: eclipLatBase
+        }
+    }
+
+    static horizontalParallaxJd(jd) {
+        const t = toJulianCenturies(jd)
+        return Moon.horizontalParallax(t)
+    }
+
+    static distance(jd) {
+        const t = toJulianCenturies(jd)
+        const theta = Moon.horizontalParallax(t)
+        return EARTH_RADIUS_KM / sin(theta)
+    }
+
+    // t in julian centuries
+    static eclipticLongBase(t) {
+        return 218.32 + 481267.881 * t +
+             6.29 * sin( 477198.87 * t + 135.0) +
+            -1.27 * sin(-413335.36 * t + 259.3) +
+             0.66 * sin( 890534.22 * t + 235.7) +
+             0.21 * sin( 954397.74 * t + 269.9) +
+            -0.19 * sin(  35999.05 * t + 357.5) +
+            -0.11 * sin( 966404.03 * t + 186.6)
+    }
+
+    static eclipticLatBase(t) {
+        const sinConstants = [
+            [5.13,  483202.02,  93.3],
+            [0.28,  960400.89,  228.2],
+            [-0.28, 6003.15,    318.3],
+            [-0.17, -407332.21, 217.6]
+        ]
+        return sum(sinConstants.map(([a, b, c]) => a * sin(b * t + c)))
+    }
+
+    static horizontalParallax(t) {
+        const cosConstants = [
+            [0.0518,    477198.85,  134.9],
+            [0.0095,    -413335.38, 259.2],
+            [0.0078,    890534.23,  235.7],
+            [0.0028,    954397.7,   269.9]
+        ] 
+
+        return 0.9508 + sum(cosConstants.map(([a, b, c]) => a * cos(b * t + c)))
+    }
+
+    static precessionCorrection(t, lat, long) {
+        const a = 1.396971 * t + 0.0003086 * t * t
+        const b = 0.013056 * t - 0.0000092 * t * t
+        const c = 5.12362 - 1.155358 * t - 0.0001964 * t * t
+
+        const latPre = lat - b * sin(long + c)
+        const longPre = long - a + b * cos(long + c) * tan(latPre)
+        return [latPre, longPre]
+    }
+}
+
+function sum(arr) {
+    let result = 0;
+    for (let a of arr) {
+        result += a;
+    }
+    return result;
+}
+
+
+
 export class Quaternions {
     // internal [s, v] - external [v, s]
     static toInternalQuat(q) {
@@ -98,28 +206,6 @@ function toAzimuth(vector4) {
 
 
 
-
-// https://en.wikipedia.org/wiki/Julian_day
-// https://github.com/mourner/suncalc/blob/master/suncalc.js 
-const millisPerDay = 1000 * 60 * 60 * 24
-const daysPerCentury = 36525
-const j2000Date = Date.UTC(2000, 0, 1, 12, 0, 0)
-const j2000jd = 2451545
-const toJulianCenturies = (jd) =>  (jd - j2000jd) / daysPerCentury
-const toJd = (date) => (date - j2000Date) / millisPerDay + j2000jd
-const mod = (m, n) => ((m%n)+n)%n 
-const rad = (deg) => deg * Math.PI / 180
-const degree = (radian) => radian * (180 / Math.PI)
-const sin = (deg) => Math.sin(rad(deg)), 
-      cos = (deg) => Math.cos(rad(deg)), 
-      tan = (deg) => Math.tan(rad(deg)), 
-      acos = (x) => degree(Math.acos(x)), 
-      asin = (x) => degree(Math.asin(x)), 
-      atan = (x) => degree(Math.atan(x)), 
-      atan2 = (x, y) => degree(Math.atan2(x, y)), 
-      PI = Math.PI; 
-
-
 // https://www.movable-type.co.uk/scripts/gis-faq-5.1.html
 // returns angle of arc subtended by earth
 // returns non-negatives value
@@ -155,18 +241,19 @@ function bearing(p1, p2) {
 // computer altitude/azimuth from ra/dec of a celestial object and its parallax angle and current time, and user location in lat long
 // celestial object is in equitorial celestial frame (not ecliptic)
 // assume parallax is 0
-function getAltAz(jd, userLoc, celestial) {
+function getAltAz(jd, userLoc, celestial, print=false) {
     userLoc = toLatLongWest(userLoc) 
-    // celestial ra from hvg dataset is in hours, so times 15 to get degrees
-    const latLongUnder = eqCelestialToLatLong(jd, celestial.ra * 15, celestial.dec)
-    //console.log('jd', jd)
-    //console.log('userLoc', userLoc)
-    //console.log('celestial', celestial)
-    //console.log('latLongUnder', latLongUnder)
+    const latLongUnder = eqCelestialToLatLong(jd, celestial.ra, celestial.dec)
 
     const angleDist = haversineDist(latLongUnder, userLoc)
 
-    //console.log('angleDist', angleDist)
+    if (print) {
+        console.log('jd', jd)
+        console.log('userLoc', userLoc)
+        console.log('celestial', celestial)
+        console.log('latLongUnder', latLongUnder)
+        console.log('angleDist', angleDist)
+    }
     const altitude = 90 - angleDist
     const azimuth = bearing(userLoc, latLongUnder)
     return { altitude, azimuth } 
@@ -224,8 +311,29 @@ const stars = await fetch('./data/stars_vis.json').then(response => response.jso
     { name: 'purple', mag: 0, dec: 0, ra: 18 } 
 ]*/
 
+const EARTH_OBLIQUITY = 23.4393 // epsilon
 
 
+function rightAscension(eclip) {
+    const l = cos(eclip.lat) * cos(eclip.long)
+    const m = 0.9175 * cos(eclip.lat) * sin(eclip.long) - 0.3978 * sin(eclip.lat)    
+    const newRes = atan(m/l) 
+    return atan2(sin(eclip.long) * cos(EARTH_OBLIQUITY) - tan(eclip.lat) * sin(EARTH_OBLIQUITY), cos(eclip.long))
+    return newRes
+}
+
+function declination(eclip) {
+    return asin(sin(eclip.lat) * cos(EARTH_OBLIQUITY) + cos(eclip.lat) * sin(EARTH_OBLIQUITY) * sin(eclip.long))
+}
+
+
+
+// modified to output ra/dec in equitorial rather than 
+function eclipticToEquitorial(eclipLatLong, jd) {
+    const ra = rightAscension(eclipLatLong)
+    const dec = declination(eclipLatLong)
+    return { ra, dec } 
+}
 
 
 
@@ -258,6 +366,38 @@ const starAzFrame3Vec = [
 
 const minMag = 4.5
 
+
+function drawStar(name, mag, xCanvas, yCanvas) {
+    ctx.beginPath();
+    // brightest star −1.46 (sirius
+    // least bright mag shown 
+    //
+    let size = -mag + minMag // between 0 and 8.46
+    
+    const magRange = 1.46 + minMag
+    const maxSize = 12
+    const minSize = 1
+    size = (size / magRange) * (maxSize - minSize) + minSize
+
+    ctx.arc(xCanvas, yCanvas, size, 0, 2 * Math.PI);
+    ctx.font = "15pt bold";
+    ctx.textAlign = "center";
+    ctx.fillText(name, xCanvas, yCanvas + 30);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+}
+
+
+function drawMoon(xCanvas, yCanvas) {
+    ctx.beginPath();
+    ctx.arc(xCanvas, yCanvas, 50, 0, 2 * Math.PI);
+    ctx.font = "15pt bold";
+    ctx.textAlign = "center";
+    ctx.fillText("Moon", xCanvas, yCanvas + 30);
+    ctx.fillStyle = 'grey';
+    ctx.fill();
+}
+
 const options = { frequency: 30, referenceFrame: "device" };
 const sensor = new AbsoluteOrientationSensor(options);
 sensor.start();
@@ -275,57 +415,49 @@ sensor.addEventListener("reading", () => {
     let renderStars = 0
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //for (const star of starAzFrame3Vec) {
+    //
+
+    const xMax = width / 2, xMin = -width /2, yMax = height / 2, yMin = -height /2
     for (const star of stars) {
-        //console.log('userLoc', userLoc)
-        //const { name, mag, x1, y1, z1 } = star
-        //const star3Vec = [x1, y1, z1]
-        
-        const { name, mag, ra, dec } = star
+        let { name, mag, ra, dec } = star
+        // celestial ra from hvg dataset is in hours, so times 15 to get degrees
+        ra *= 15
+
         const { altitude: starAlt, azimuth: starAz} = getAltAz(jd, userLatLong, { ra, dec })
         const star3Vec = to3Vec(starAlt, starAz, radius)
-
-
-        //console.log('vec3', name, star3Vec)
         const [_, x, y, z] = Quaternions.rotate(star3Vec, inverseOrientQuat)
-
-
-        const xMax = width / 2, xMin = -width /2, yMax = height / 2, yMin = -height /2
         const inFrame = z < 0 && xMin <= x && x <= xMax && yMin <= y && y <= yMax
-            
-
-        //console.log('rotated', name, x, y, z)
-       
         if (mag < minMag && inFrame) {
             renderStars += 1
             const xCanvas = x + width / 2
             const yCanvas = -y + height / 2
-
-
-            ctx.beginPath();
-
-
-            // brightest star −1.46 (sirius
-            // least bright mag shown 
-            //
-            let size = -mag + minMag // between 0 and 8.46
-            
-            const magRange = 1.46 + minMag
-            const maxSize = 12
-            const minSize = 1
-            size = (size / magRange) * (maxSize - minSize) + minSize
-            
-            ctx.arc(xCanvas, yCanvas, size, 0, 2 * Math.PI);
-
-            ctx.font = "15pt bold";
-            ctx.textAlign = "center";
-            ctx.fillText(name, xCanvas, yCanvas + 30);
-        
-            ctx.fillStyle = 'white';
-            ctx.fill();
+            drawStar(name, mag, xCanvas, yCanvas)
         }
-
-        //console.log(name, x, y, inFrame)
     }
+
+    console.log('putz')
+
+    const moonEclipLL = Moon.eclipLatLong(jd)
+    const moonLoc = eclipticToEquitorial(moonEclipLL, jd)
+    const { altitude: moonAlt, azimuth: moonAz} = getAltAz(jd, userLatLong, moonLoc, true)
+    const moon3Vec = to3Vec(moonAlt, moonAz, radius)
+    const [_, mx, my, mz] = Quaternions.rotate(moon3Vec, inverseOrientQuat)
+
+
+    console.log('moonEclipLL', moonEclipLL)
+    console.log('moonLoc', moonLoc)
+    console.log('moon alt/az', moonAlt, moonAz)
+    console.log('moon3Vec', moon3Vec)
+
+    if (mz < 0) {
+        const xmCanvas = mx + width / 2
+        const ymCanvas = -my + height / 2
+    console.log('moon canvas', xmCanvas, ymCanvas)
+        drawMoon(xmCanvas, ymCanvas)
+    }
+
+
+
 
     console.log('totalStars', totalStars)
     console.log('renderStars', renderStars)

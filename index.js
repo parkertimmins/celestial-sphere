1c08010850684698de22bc90bdb16492a7dd4b0a
parkertimmins <parkertimmins@gmail.com>
2023-12-30 13:14:58 -0600

    Add more logging for iPhone, cause it refuses to work!

--- index.js
+++ index.js
@@ -365,6 +365,7 @@ function iosRenderOnOrientChange() {
                 state.bearingDiffFilter.update(bearingDiff)
                 const northOffsetQuat = Quaternions.fromAngleAxis(state.bearingDiffFilter.value, [0, 0, -1])
                 state.orientQuat = Quaternions.multiply(northOffsetQuat, relativeQuat)
+                console.log('bearing', bearingDiff, bearingDiffFilter.value)
                 render(state, ctx, canvas)
             });
           }

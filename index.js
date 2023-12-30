c51d578e182da55ba172eb3b80d6966e9c235f63
parkertimmins <parkertimmins@gmail.com>
2023-12-30 14:37:28 -0600

    Iphone flips at horizon, add more logging

--- index.js
+++ index.js
@@ -365,6 +365,7 @@ function iosRenderOnOrientChange() {
                 state.bearingDiffFilter.update(bearingDiff)
                 const northOffsetQuat = Quaternions.fromAngleAxis(state.bearingDiffFilter.value, [0, 0, -1])
                 state.orientQuat = Quaternions.multiply(northOffsetQuat, relativeQuat)
+                console.log(event.webkitCompassHeading, bearingDiff, bearingRelativeNorth, state.bearingDiffFilter.value)
                 render(state, ctx, canvas)
             });
           }

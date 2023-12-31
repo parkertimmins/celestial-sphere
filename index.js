912fb224a17997192612396d248a2062f07ea81e
parkertimmins <parkertimmins@gmail.com>
2023-12-31 10:31:26 -0600

    remove logging

--- index.js
+++ index.js
@@ -365,7 +365,7 @@ function iosRenderOnOrientChange() {
                 state.bearingDiffFilter.update(bearingDiff)
                 const northOffsetQuat = Quaternions.fromAngleAxis(state.bearingDiffFilter.value, [0, 0, -1])
                 state.orientQuat = Quaternions.multiply(northOffsetQuat, relativeQuat)
-                console.log(event.webkitCompassHeading, bearingRelativeNorth, bearingDiff, state.bearingDiffFilter.value)
+                //console.log(event.webkitCompassHeading, bearingRelativeNorth, bearingDiff, state.bearingDiffFilter.value)
                 render(state, ctx, canvas)
             }, true);
           }

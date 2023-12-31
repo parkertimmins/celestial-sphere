e0ca394d29e2283bcdfe025820adc3f0ccc5bd4e
parkertimmins <parkertimmins@gmail.com>
2023-12-31 15:10:27 -0600

    Lots of spinning near zenith, adding logging

--- index.js
+++ index.js
@@ -367,10 +367,9 @@ function iosRenderOnOrientChange() {
                 const bearingRelativeNorth = thetaToAz(thetaRelativeNorth)
                 const bearingDiff = mod(event.webkitCompassHeading - bearingRelativeNorth, 360)
                 state.bearingDiffFilter.update(bearingDiff, percHorizontalComponents)
-                //console.log(event.webkitCompassHeading, event.webkitCompassAccuracy, bearingDiff, bearingRelativeNorth)
+                console.log(event.webkitCompassHeading, bearingDiff, bearingRelativeNorth, percHorizontalComponentst)
                 const northOffsetQuat = Quaternions.fromAngleAxis(state.bearingDiffFilter.value, [0, 0, -1])
                 state.orientQuat = Quaternions.multiply(northOffsetQuat, relativeQuat)
-                //console.log(event.webkitCompassHeading, bearingRelativeNorth, bearingDiff, state.bearingDiffFilter.value)
                 render(state, ctx, canvas)
             }, true);
           }
